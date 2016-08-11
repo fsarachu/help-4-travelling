5ba94f0d359ed16f2307c96b081a69569b1e0966
Francisco Villegas <fcvillegas@gmail.com>
2016-08-10 23:38:10 -0300

    estructura proyecto maven con modules

---
+++ estacion-de-trabajo/src/main/java/uy/edu/cure/estacion/de/trabajo/App.java
@@ -0,0 +1,13 @@
+package uy.edu.cure.estacion.de.trabajo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ servidor-web/src/main/java/uy/edu/cure/servidor/web/App.java
@@ -0,0 +1,12 @@
+package uy.edu.cure.servidor.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
