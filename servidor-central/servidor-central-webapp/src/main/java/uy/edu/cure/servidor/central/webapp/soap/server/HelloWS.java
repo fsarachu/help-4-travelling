e42a9a5b7b5875618ba3abea1c94f54ca34c36b8
Francisco Villegas <fcvillegas@gmail.com>
2016-08-13 23:02:37 -0300

    soap web service

--- servidor-central/servidor-central-webapp/src/main/java/uy/edu/cure/servidor/central/webapp/soap/server/HelloWS.java
+++ servidor-central/servidor-central-webapp/src/main/java/uy/edu/cure/servidor/central/webapp/soap/server/HelloWS.java
@@ -3,13 +3,9 @@ package uy.edu.cure.servidor.central.webapp.soap.server;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
-// http://localhost:8080/servidor-central-webapp/HelloWSService?wsdl
 @WebService
-public class HelloWS {
-    private String message = new String("Hello, ");
+public interface HelloWS {
 
-    @WebMethod
-    public String sayHello(String name) {
-        return message + name + ".";
-    }
+    @WebMethod public String sayHello(String name);
+    
 }
---
+++ servidor-central/servidor-central-webapp/src/main/java/uy/edu/cure/servidor/central/webapp/soap/server/HelloWSImpl.java
@@ -0,0 +1,14 @@
+package uy.edu.cure.servidor.central.webapp.soap.server;
+
+import javax.jws.WebService;
+
+// http://localhost:8080/servidor-central-webapp/HelloWSService?wsdl
+@WebService(endpointInterface = "uy.edu.cure.servidor.central.webapp.soap.server.HelloWS")
+public class HelloWSImpl implements HelloWS {
+	private String message = new String("Hello, ");
+
+	@Override
+	public String sayHello(String name) {
+		return message + name + ".";
+	}
+}
