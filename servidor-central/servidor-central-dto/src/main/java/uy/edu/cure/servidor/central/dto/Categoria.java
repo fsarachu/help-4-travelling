556fd82e724ef7707d4b5a4262451d534d08b7a1
Victor Torterola <tricovictor@gmail.com>
2016-08-22 20:46:34 -0300

    Se agregan Getter y Setter de Categorias

--- servidor-central/servidor-central-dto/src/main/java/uy/edu/cure/servidor/central/dto/Categoria.java
+++ servidor-central/servidor-central-dto/src/main/java/uy/edu/cure/servidor/central/dto/Categoria.java
@@ -7,4 +7,30 @@ import java.util.ArrayList;
  */
 public class Categoria {
     private String nombre;
+    private Categoria padre;
+    private ArrayList<Categoria> hijos;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Categoria getPadre() {
+        return padre;
+    }
+
+    public void setPadre(Categoria padre) {
+        this.padre = padre;
+    }
+
+    public ArrayList<Categoria> getHijos() {
+        return hijos;
+    }
+
+    public void setHijos(ArrayList<Categoria> hijos) {
+        this.hijos = hijos;
+    }
 }
