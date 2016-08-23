3f623c60cf0e0d7dc089e2f345a748ec34732993
Victor Torterola <tricovictor@gmail.com>
2016-08-22 21:41:34 -0300

    Implementamos clienteServiceImpl

--- servidor-central/servidor-central-lib/src/main/java/uy/edu/cure/servidor/central/lib/servicios/ClienteService.java
+++ servidor-central/servidor-central-lib/src/main/java/uy/edu/cure/servidor/central/lib/servicios/ClienteService.java
@@ -1,7 +1,14 @@
 package uy.edu.cure.servidor.central.lib.servicios;
 
+import uy.edu.cure.servidor.central.dto.Usuario;
+
 /**
  * Created by victor on 22/08/16.
  */
 public interface ClienteService {
+    public void agregar(Usuario usuario);
+    public void eliminar(Usuario usuario);
+    public void modificar(Usuario anterior, Usuario nuevo);
+    public Usuario buscar(Usuario usuario);
+
 }
--- servidor-central/servidor-central-lib/src/main/java/uy/edu/cure/servidor/central/lib/servicios/memoria/ClienteServiceImpl.java
+++ servidor-central/servidor-central-lib/src/main/java/uy/edu/cure/servidor/central/lib/servicios/memoria/ClienteServiceImpl.java
@@ -1,5 +1,26 @@
 package uy.edu.cure.servidor.central.lib.servicios.memoria;
 
-public class ClienteServiceImpl {
-    public void agregar()
+import uy.edu.cure.servidor.central.dto.Usuario;
+import uy.edu.cure.servidor.central.lib.servicios.ClienteService;
+
+public class ClienteServiceImpl implements ClienteService {
+    @Override
+    public void agregar(Usuario usuario) {
+
+    }
+
+    @Override
+    public void eliminar(Usuario usuario) {
+
+    }
+
+    @Override
+    public void modificar(Usuario anterior, Usuario nuevo) {
+
+    }
+
+    @Override
+    public Usuario buscar(Usuario usuario) {
+        return null;
+    }
 }
