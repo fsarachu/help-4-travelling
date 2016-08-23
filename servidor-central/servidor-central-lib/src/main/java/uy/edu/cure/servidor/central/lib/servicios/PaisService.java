909c745d9a2bfe951e05aaeaae5f40cc990af80f
Victor Torterola <tricovictor@gmail.com>
2016-08-22 21:52:46 -0300

    Implementamos los faltantes

--- servidor-central/servidor-central-lib/src/main/java/uy/edu/cure/servidor/central/lib/servicios/PaisService.java
+++ servidor-central/servidor-central-lib/src/main/java/uy/edu/cure/servidor/central/lib/servicios/PaisService.java
@@ -1,7 +1,13 @@
 package uy.edu.cure.servidor.central.lib.servicios;
 
+import uy.edu.cure.servidor.central.dto.Pais;
+
 /**
  * Created by victor on 22/08/16.
  */
 public interface PaisService {
+    public void agregar(Pais pais);
+    public void eliminar(Pais pais);
+    public void modificar(Pais anterior, Pais nuevo);
+    public Pais buscar(Pais pais);
 }
--- servidor-central/servidor-central-lib/src/main/java/uy/edu/cure/servidor/central/lib/servicios/ProveedorService.java
+++ servidor-central/servidor-central-lib/src/main/java/uy/edu/cure/servidor/central/lib/servicios/ProveedorService.java
@@ -1,7 +1,13 @@
 package uy.edu.cure.servidor.central.lib.servicios;
 
+import uy.edu.cure.servidor.central.dto.Proveedor;
+
 /**
  * Created by victor on 22/08/16.
  */
 public interface ProveedorService {
+    public void agregar(Proveedor proveedor);
+    public void eliminar(Proveedor proveedor);
+    public void modificar(Proveedor anterior, Proveedor nuevo);
+    public Proveedor buscar(Proveedor proveedor);
 }
