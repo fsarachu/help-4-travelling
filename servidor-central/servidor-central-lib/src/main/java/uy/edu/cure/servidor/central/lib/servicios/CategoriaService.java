6ad1e0f832444815364997796a2a98e67e84978d
Victor Torterola <tricovictor@gmail.com>
2016-08-22 21:56:00 -0300

    Implementamos ServicioServiceImpl

--- servidor-central/servidor-central-lib/src/main/java/uy/edu/cure/servidor/central/lib/servicios/CategoriaService.java
+++ servidor-central/servidor-central-lib/src/main/java/uy/edu/cure/servidor/central/lib/servicios/CategoriaService.java
@@ -2,13 +2,9 @@ package uy.edu.cure.servidor.central.lib.servicios;
 
 import uy.edu.cure.servidor.central.dto.Categoria;
 
-/**
- * Created by victor on 22/08/16.
- */
 public interface CategoriaService {
     public void agregar(Categoria categoria);
     public void eliminar(Categoria categoria);
     public void modificar(Categoria anterior, Categoria nuevo);
     public Categoria buscar(Categoria categoria);
-
 }
--- servidor-central/servidor-central-lib/src/main/java/uy/edu/cure/servidor/central/lib/servicios/memoria/ServicioServiceImpl.java
+++ servidor-central/servidor-central-lib/src/main/java/uy/edu/cure/servidor/central/lib/servicios/memoria/ServicioServiceImpl.java
@@ -1,4 +1,26 @@
 package uy.edu.cure.servidor.central.lib.servicios.memoria;
 
-public class ServicioServiceImpl {
+import uy.edu.cure.servidor.central.dto.Servicio;
+import uy.edu.cure.servidor.central.lib.servicios.ServicioService;
+
+public class ServicioServiceImpl implements ServicioService {
+    @Override
+    public void agregar(Servicio servicio) {
+        
+    }
+
+    @Override
+    public void eliminar(Servicio servicio) {
+
+    }
+
+    @Override
+    public void modificar(Servicio anterior, Servicio nuevo) {
+
+    }
+
+    @Override
+    public Servicio buscar(Servicio servicio) {
+        return null;
+    }
 }
