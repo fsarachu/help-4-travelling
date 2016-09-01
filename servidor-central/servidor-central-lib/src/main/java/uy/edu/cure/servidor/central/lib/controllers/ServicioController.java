d3388ae5e7081c6e6763dcdaaedb45acc5436ec5
Franco Sarachu <fsarachu@gmail.com>
2016-09-01 20:24:17 -0300

    Implemento CU3 - Alta de Servicio

---
+++ servidor-central/servidor-central-lib/src/main/java/uy/edu/cure/servidor/central/lib/controllers/ServicioController.java
@@ -0,0 +1,19 @@
+package uy.edu.cure.servidor.central.lib.controllers;
+
+import uy.edu.cure.servidor.central.dto.Proveedor;
+import uy.edu.cure.servidor.central.dto.Servicio;
+import uy.edu.cure.servidor.central.lib.servicios.ServiceFactory;
+import uy.edu.cure.servidor.central.lib.servicios.ServicioService;
+
+public class ServicioController {
+    public void alta(Servicio servicio) {
+        ServicioService servicioService = ServiceFactory.getServicioService();
+
+        servicioService.agregar(servicio);
+
+        Proveedor proveedor = servicio.getProveedor();
+
+        proveedor.getServicios().add(servicio);
+
+    }
+}
