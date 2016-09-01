cdb40cc1e1f7f2e80887a46bc8ec185106f72aa3
Franco Sarachu <fsarachu@gmail.com>
2016-09-01 20:36:48 -0300

    Implemento CU4 - Alta de Promoción

---
+++ servidor-central/servidor-central-lib/src/main/java/uy/edu/cure/servidor/central/lib/controllers/PromocionController.java
@@ -0,0 +1,18 @@
+package uy.edu.cure.servidor.central.lib.controllers;
+
+import uy.edu.cure.servidor.central.dto.Promocion;
+import uy.edu.cure.servidor.central.dto.Proveedor;
+import uy.edu.cure.servidor.central.lib.servicios.PromocionService;
+import uy.edu.cure.servidor.central.lib.servicios.ServiceFactory;
+
+public class PromocionController {
+    public void alta(Promocion promocion) {
+        PromocionService promocionService = ServiceFactory.getPromocionService();
+
+        promocionService.agregar(promocion);
+
+        Proveedor proveedor = promocion.getProveedor();
+
+        proveedor.getPromociones().add(promocion);
+    }
+}
