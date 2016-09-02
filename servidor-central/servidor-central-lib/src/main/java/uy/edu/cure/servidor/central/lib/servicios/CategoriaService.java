f63b5917e81e0d3e1d6644ebffac0a5c2b61d678
Franco Sarachu <fsarachu@gmail.com>
2016-09-01 22:09:48 -0300

    Refactor CategoriaService para heredar de GenericService

--- servidor-central/servidor-central-lib/src/main/java/uy/edu/cure/servidor/central/lib/servicios/CategoriaService.java
+++ servidor-central/servidor-central-lib/src/main/java/uy/edu/cure/servidor/central/lib/servicios/CategoriaService.java
@@ -3,11 +3,11 @@ package uy.edu.cure.servidor.central.lib.servicios;
 import uy.edu.cure.servidor.central.dto.Categoria;
 
 public interface CategoriaService {
-    void agregar(Categoria categoria);
+    void agregar(Integer id, Categoria categoria);
 
-    void eliminar(Categoria categoria);
+    void eliminar(Integer id);
 
-    void modificar(Categoria anterior, Categoria nuevo);
+    void modificar(Integer id, Categoria nuevo);
 
-    Categoria obtener(String nombre);
+    Categoria obtener(int id);
 }
