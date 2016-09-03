13354a222dca090af75ea70b6478e91af2015f24
Franco Sarachu <fsarachu@gmail.com>
2016-09-03 17:20:56 -0300

    Agrego Default Constructor en Usuario

--- servidor-central/servidor-central-dto/src/main/java/uy/edu/cure/servidor/central/dto/Cliente.java
+++ servidor-central/servidor-central-dto/src/main/java/uy/edu/cure/servidor/central/dto/Cliente.java
@@ -5,6 +5,9 @@ import java.awt.image.BufferedImage;
 import java.util.Date;
 
 public class Cliente extends Usuario {
+    public Cliente() {
+    }
+
     public Cliente(Integer id, String nickname, String nombre, String apellido, String correo, Date fechaNacimiento, BufferedImage imagen) {
         super(id, nickname, nombre, apellido, correo, fechaNacimiento, imagen);
     }
