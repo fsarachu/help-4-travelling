e371bb2636cc325773a04f08abcd83e265f474e6
Franco Sarachu <fsarachu@gmail.com>
2016-09-03 17:07:56 -0300

    Agrego constructores y nueva lista de ids en DTO Servicio

--- servidor-central/servidor-central-dto/src/main/java/uy/edu/cure/servidor/central/dto/Servicio.java
+++ servidor-central/servidor-central-dto/src/main/java/uy/edu/cure/servidor/central/dto/Servicio.java
@@ -5,9 +5,24 @@ import java.util.ArrayList;
 
 public class Servicio extends Producto {
     private ArrayList<BufferedImage> imagenes;
+    private ArrayList<Integer> idCategorias;
     private ArrayList<Categoria> categorias;
     private Ciudad origen;
     private Ciudad destino;
+
+    public Servicio() {
+        this.imagenes = new ArrayList<>();
+        this.idCategorias = new ArrayList<>();
+        this.categorias = new ArrayList<>();
+    }
+
+    public Servicio(ArrayList<BufferedImage> imagenes, ArrayList<Integer> idCategorias, ArrayList<Categoria> categorias, Ciudad origen, Ciudad destino) {
+        this.imagenes = imagenes;
+        this.idCategorias = idCategorias;
+        this.categorias = categorias;
+        this.origen = origen;
+        this.destino = destino;
+    }
 
     public ArrayList<BufferedImage> getImagenes() {
         return imagenes;
@@ -15,6 +30,14 @@ public class Servicio extends Producto {
 
     public void setImagenes(ArrayList<BufferedImage> imagenes) {
         this.imagenes = imagenes;
+    }
+
+    public ArrayList<Integer> getIdCategorias() {
+        return idCategorias;
+    }
+
+    public void setIdCategorias(ArrayList<Integer> idCategorias) {
+        this.idCategorias = idCategorias;
     }
 
     public ArrayList<Categoria> getCategorias() {
