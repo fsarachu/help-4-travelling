6512786eb1b1642a24f3eb69b9fb1755e7760105
Franco Sarachu <fsarachu@gmail.com>
2016-09-05 11:20:47 -0300

    Agrego metodo listar a ClienteController

--- servidor-central/servidor-central-lib/src/main/java/uy/edu/cure/servidor/central/lib/controllers/ClienteController.java
+++ servidor-central/servidor-central-lib/src/main/java/uy/edu/cure/servidor/central/lib/controllers/ClienteController.java
@@ -4,10 +4,18 @@ import uy.edu.cure.servidor.central.dto.Cliente;
 import uy.edu.cure.servidor.central.lib.servicios.ClienteService;
 import uy.edu.cure.servidor.central.lib.servicios.ServiceFactory;
 
+import java.util.ArrayList;
+
 public class ClienteController {
     public void alta(Cliente cliente) {
         ClienteService clienteService = ServiceFactory.getClienteService();
 
         clienteService.agregar(cliente.getId(), cliente);
+    }
+
+    public ArrayList<Cliente> listar() {
+        ClienteService clienteService = ServiceFactory.getClienteService();
+
+        return clienteService.listar();
     }
 }
