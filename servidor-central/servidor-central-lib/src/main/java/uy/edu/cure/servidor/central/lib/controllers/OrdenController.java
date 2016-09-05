d8ad220f6b503da1316e29c9a7130aa41136ee15
Franco Sarachu <fsarachu@gmail.com>
2016-09-05 10:41:07 -0300

    Creo OrdenController

---
+++ servidor-central/servidor-central-lib/src/main/java/uy/edu/cure/servidor/central/lib/controllers/OrdenController.java
@@ -0,0 +1,10 @@
+package uy.edu.cure.servidor.central.lib.controllers;
+
+import uy.edu.cure.servidor.central.dto.Orden;
+import uy.edu.cure.servidor.central.lib.servicios.OrdenService;
+import uy.edu.cure.servidor.central.lib.servicios.ServiceFactory;
+
+public class OrdenController {
+    public void nueva(Orden orden) {
+    }
+}
