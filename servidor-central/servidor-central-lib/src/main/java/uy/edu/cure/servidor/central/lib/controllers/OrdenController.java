1f68ed5fb798b86f828c0d7eca4b0ab2e2966b28
Franco Sarachu <fsarachu@gmail.com>
2016-09-05 11:38:46 -0300

    Agrego metodo listar a todos los controllers

--- servidor-central/servidor-central-lib/src/main/java/uy/edu/cure/servidor/central/lib/controllers/OrdenController.java
+++ servidor-central/servidor-central-lib/src/main/java/uy/edu/cure/servidor/central/lib/controllers/OrdenController.java
@@ -4,7 +4,17 @@ import uy.edu.cure.servidor.central.dto.Orden;
 import uy.edu.cure.servidor.central.lib.servicios.OrdenService;
 import uy.edu.cure.servidor.central.lib.servicios.ServiceFactory;
 
+import java.util.ArrayList;
+
 public class OrdenController {
+
+
     public void nueva(Orden orden) {
+    }
+
+    public ArrayList<Orden> listar() {
+        OrdenService ordenService = ServiceFactory.getOrdenService();
+
+        return ordenService.listar();
     }
 }
--- servidor-central/servidor-central-lib/src/main/java/uy/edu/cure/servidor/central/lib/controllers/ServicioController.java
+++ servidor-central/servidor-central-lib/src/main/java/uy/edu/cure/servidor/central/lib/controllers/ServicioController.java
@@ -5,6 +5,8 @@ import uy.edu.cure.servidor.central.dto.Servicio;
 import uy.edu.cure.servidor.central.lib.servicios.ServiceFactory;
 import uy.edu.cure.servidor.central.lib.servicios.ServicioService;
 
+import java.util.ArrayList;
+
 public class ServicioController {
     public void alta(Servicio servicio) {
         ServicioService servicioService = ServiceFactory.getServicioService();
@@ -15,5 +17,11 @@ public class ServicioController {
 
         proveedor.getServicios().add(servicio);
 
+    }
+
+    public ArrayList<Servicio> listar() {
+        ServicioService servicioService = ServiceFactory.getServicioService();
+
+        return servicioService.listar();
     }
 }
