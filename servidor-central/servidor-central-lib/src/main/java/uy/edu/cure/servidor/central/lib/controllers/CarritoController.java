00e3a43b1d0de86d8b597d2101509bc2e1cc4841
Franco Sarachu <fsarachu@gmail.com>
2016-09-14 20:18:53 -0300

    Agrego metodo eliminarItem()

--- servidor-central/servidor-central-lib/src/main/java/uy/edu/cure/servidor/central/lib/controllers/CarritoController.java
+++ servidor-central/servidor-central-lib/src/main/java/uy/edu/cure/servidor/central/lib/controllers/CarritoController.java
@@ -4,13 +4,16 @@ import uy.edu.cure.servidor.central.dto.Carrito;
 import uy.edu.cure.servidor.central.dto.Cliente;
 import uy.edu.cure.servidor.central.dto.ItemReserva;
 import uy.edu.cure.servidor.central.lib.servicios.CarritoService;
+import uy.edu.cure.servidor.central.lib.servicios.ItemReservaService;
 import uy.edu.cure.servidor.central.lib.servicios.ServiceFactory;
 
 public class CarritoController {
     private CarritoService carritoService;
+    private ItemReservaService itemReservaService;
 
     public CarritoController() {
         this.carritoService = ServiceFactory.getCarritoService();
+        this.itemReservaService = ServiceFactory.getItemReservaService();
     }
 
     public Carrito obtenerCarrito(Integer idCarrito) {
@@ -22,4 +25,9 @@ public class CarritoController {
         carrito.getItems().add(nuevoItem);
         carrito.actualizarTotal();
     }
+
+    public void eliminarItem(Integer idItem) {
+        this.itemReservaService.eliminar(idItem);
+    }
+
 }
