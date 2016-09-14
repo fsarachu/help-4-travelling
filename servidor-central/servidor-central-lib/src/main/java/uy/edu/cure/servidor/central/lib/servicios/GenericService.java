431b6b9453aebb48b034b273d2b98a30fbe00ad9
Franco Sarachu <fsarachu@gmail.com>
2016-09-14 16:33:14 -0300

    Add GenericService Interface

---
+++ servidor-central/servidor-central-lib/src/main/java/uy/edu/cure/servidor/central/lib/servicios/GenericService.java
@@ -0,0 +1,15 @@
+package uy.edu.cure.servidor.central.lib.servicios;
+
+import java.util.ArrayList;
+
+public interface GenericService<T> {
+    void agregar(Integer id, T obj);
+
+    void eliminar(Integer id);
+
+    void modificar(Integer id, T obj);
+
+    T obtener(Integer id);
+
+    ArrayList<T> listar();
+}
