12556b309eb2eece323ce528353ded7707174117
Franco Sarachu <fsarachu@gmail.com>
2016-09-14 21:00:30 -0300

    Implemento metodo nueva reserva

--- servidor-central/servidor-central-lib/src/main/java/uy/edu/cure/servidor/central/lib/controllers/ReservaController.java
+++ servidor-central/servidor-central-lib/src/main/java/uy/edu/cure/servidor/central/lib/controllers/ReservaController.java
@@ -1,19 +1,44 @@
 package uy.edu.cure.servidor.central.lib.controllers;
 
+import uy.edu.cure.servidor.central.dto.Carrito;
+import uy.edu.cure.servidor.central.dto.Cliente;
+import uy.edu.cure.servidor.central.dto.EstadoReserva;
 import uy.edu.cure.servidor.central.dto.Reserva;
+import uy.edu.cure.servidor.central.lib.servicios.CarritoService;
 import uy.edu.cure.servidor.central.lib.servicios.ReservaService;
 import uy.edu.cure.servidor.central.lib.servicios.ServiceFactory;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class ReservaController {
 
-    public void nueva(Reserva reserva) {
+    private ReservaService reservaService;
+    private CarritoService carritoService;
+
+    public ReservaController() {
+        this.reservaService = ServiceFactory.getReservaService();
+        this.carritoService = ServiceFactory.getCarritoService();
     }
 
     public ArrayList<Reserva> listar() {
-        ReservaService reservaService = ServiceFactory.getReservaService();
+        return this.reservaService.listar();
+    }
 
-        return reservaService.listar();
+    public void nueva(Cliente cliente) {
+        Reserva reserva = new Reserva();
+
+        reserva.setId(this.reservaService.nextId());
+        reserva.setCliente(cliente);
+        reserva.setCarrito(cliente.getCarrito());
+        reserva.setFechaCreacion(new Date());
+        reserva.setEstado(EstadoReserva.registrada);
+
+        Carrito nuevoCarrito = new Carrito();
+        nuevoCarrito.setId(this.carritoService.nextId());
+        nuevoCarrito.setCliente(cliente);
+        this.carritoService.agregar(nuevoCarrito.getId(), nuevoCarrito);
+
+        cliente.setCarrito(nuevoCarrito);
     }
 }
