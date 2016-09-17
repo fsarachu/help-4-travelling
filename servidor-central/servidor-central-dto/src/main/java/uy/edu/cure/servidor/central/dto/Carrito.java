8f01429a4fb97aa11708966b891f6edb3c1f9c47
Franco Sarachu <fsarachu@gmail.com>
2016-09-17 19:07:50 -0300

    Refactor CarritoController

--- servidor-central/servidor-central-dto/src/main/java/uy/edu/cure/servidor/central/dto/Carrito.java
+++ servidor-central/servidor-central-dto/src/main/java/uy/edu/cure/servidor/central/dto/Carrito.java
@@ -4,17 +4,20 @@ import java.util.ArrayList;
 
 public class Carrito {
     private Integer id;
+    private ArrayList<Integer> idItems;
     private ArrayList<ItemReserva> items;
     private Cliente cliente;
     private double total;
 
     public Carrito() {
+        this.idItems = new ArrayList<>();
         this.items = new ArrayList<>();
         this.total = 0.0;
     }
 
-    public Carrito(Integer id, ArrayList<ItemReserva> items, Cliente cliente, double total) {
+    public Carrito(Integer id, ArrayList<Integer> idItems, ArrayList<ItemReserva> items, Cliente cliente, double total) {
         this.id = id;
+        this.idItems = idItems;
         this.items = items;
         this.cliente = cliente;
         this.total = total;
@@ -26,6 +29,14 @@ public class Carrito {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public ArrayList<Integer> getIdItems() {
+        return idItems;
+    }
+
+    public void setIdItems(ArrayList<Integer> idItems) {
+        this.idItems = idItems;
     }
 
     public ArrayList<ItemReserva> getItems() {
@@ -50,15 +61,5 @@ public class Carrito {
 
     public void setTotal(double total) {
         this.total = total;
-    }
-
-    public void actualizarTotal() {
-        double newTotal = 0.0;
-
-        for (ItemReserva item : this.items) {
-            newTotal += item.getSubTotal();
-        }
-
-        this.total = newTotal;
     }
 }
