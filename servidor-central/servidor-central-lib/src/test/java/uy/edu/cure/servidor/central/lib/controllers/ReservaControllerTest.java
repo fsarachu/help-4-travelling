ee5a653ad701966a7c0a0484af68c1a1b042411d
Franco Sarachu <fsarachu@gmail.com>
2016-09-19 12:33:16 -0300

    Sigo reescribiendo ReservaControllerTest

--- servidor-central/servidor-central-lib/src/test/java/uy/edu/cure/servidor/central/lib/controllers/ReservaControllerTest.java
+++ servidor-central/servidor-central-lib/src/test/java/uy/edu/cure/servidor/central/lib/controllers/ReservaControllerTest.java
@@ -1,20 +1,83 @@
 package uy.edu.cure.servidor.central.lib.controllers;
 
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import uy.edu.cure.servidor.central.dto.Cliente;
+import uy.edu.cure.servidor.central.dto.Reserva;
 import uy.edu.cure.servidor.central.lib.servicios.ReservaService;
 import uy.edu.cure.servidor.central.lib.servicios.ServiceFactory;
+
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
 
 public class ReservaControllerTest {
 
     private static ReservaController reservaController;
     private static ReservaService reservaService;
+    private static ClienteController clienteController;
 
 
     @BeforeClass
     public static void beforeAll() {
         reservaController = new ReservaController();
         reservaService = ServiceFactory.getReservaService();
+        clienteController = new ClienteController();
+    }
+
+    @Before
+    public void beforeEach() {
+        reservaService.vaciar();
+    }
+
+    @Test
+    public void nueva() throws Exception {
+        Cliente cliente = new Cliente();
+        clienteController.nuevo(cliente);
+
+        List<Reserva> reservas = reservaService.listarReservasCliente(cliente);
+
+        assertTrue(reservas.size() == 0);
+
+        reservaController.nueva(cliente);
+
+        reservas = reservaService.listarReservasCliente(cliente);
+
+        assertTrue(reservas.size() == 1);
+    }
+
+    @Test
+    public void obtener() throws Exception {
+        Cliente cliente = new Cliente();
+        clienteController.nuevo(cliente);
+
+        reservaController.nueva(cliente);
+
+        Reserva reserva = reservaService.listarReservasCliente(cliente).get(0);
+
+        assertEquals(reserva, reservaController.obtener(reserva.getId()));
+    }
+
+    @Test
+    public void modificar() throws Exception {
+        Cliente cliente = new Cliente();
+        clienteController.nuevo(cliente);
+
+        reservaController.nueva(cliente);
+
+        Reserva reservaOld = reservaService.listarReservasCliente(cliente).get(0);
+
+        assertEquals(reservaOld, reservaController.obtener(reservaOld.getId()));
+
+        Reserva reservaNew = new Reserva();
+        reservaNew.setId(reservaOld.getId());
+        reservaNew.setCliente(reservaOld.getCliente());
+
+        reservaController.modificar(reservaNew);
+
+        assertEquals(reservaNew, reservaController.obtener(reservaOld.getId()));
     }
 
     @Test
@@ -23,22 +86,7 @@ public class ReservaControllerTest {
     }
 
     @Test
-    public void obtener() throws Exception {
-
-    }
-
-    @Test
-    public void modificar() throws Exception {
-
-    }
-
-    @Test
     public void listar() throws Exception {
-
-    }
-
-    @Test
-    public void nueva() throws Exception {
 
     }
 
