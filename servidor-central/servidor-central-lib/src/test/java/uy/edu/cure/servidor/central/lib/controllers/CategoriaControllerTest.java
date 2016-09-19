8b74f750e058c72c989fae07a9cc88a4c5d820cc
Franco Sarachu <fsarachu@gmail.com>
2016-09-19 20:07:55 -0300

    Implemento testOcultar

--- servidor-central/servidor-central-lib/src/test/java/uy/edu/cure/servidor/central/lib/controllers/CategoriaControllerTest.java
+++ servidor-central/servidor-central-lib/src/test/java/uy/edu/cure/servidor/central/lib/controllers/CategoriaControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import uy.edu.cure.servidor.central.dto.Categoria;
+import uy.edu.cure.servidor.central.dto.EstadoCategoria;
 import uy.edu.cure.servidor.central.lib.servicios.CategoriaService;
 import uy.edu.cure.servidor.central.lib.servicios.ServiceFactory;
 
@@ -67,7 +68,7 @@ public class CategoriaControllerTest {
         List<Categoria> expected = new ArrayList<>();
 
         for (int i = 0; i < 3; i++) {
-            expected.add(new Categoria(null, null, null, null));
+            expected.add(new Categoria(null, null, null, null, null));
         }
 
         for (Categoria categoria : expected) {
@@ -80,4 +81,72 @@ public class CategoriaControllerTest {
 
     }
 
+    @Test
+    public void ocultar() throws Exception {
+        ArrayList<Categoria> categoriasExpected = new ArrayList<>();
+
+        Categoria categoria1 = new Categoria();
+        categoria1.setNombre("Vuelos");
+        categoriasExpected.add(categoria1);
+
+        Categoria categoria1_1 = new Categoria();
+        categoria1_1.setNombre("Rapidos");
+        categoria1_1.setPadre(categoria1);
+        categoria1.getHijos().add(categoria1_1);
+        categoriasExpected.add(categoria1_1);
+
+        Categoria categoria1_2 = new Categoria();
+        categoria1_2.setNombre("Lentos");
+        categoria1_2.setPadre(categoria1);
+        categoria1.getHijos().add(categoria1_2);
+        categoriasExpected.add(categoria1_2);
+
+        Categoria categoria2 = new Categoria();
+        categoria2.setNombre("Hoteles");
+        categoriasExpected.add(categoria2);
+
+        Categoria categoria2_1 = new Categoria();
+        categoria2_1.setNombre("Feos");
+        categoria2_1.setPadre(categoria2);
+        categoria2.getHijos().add(categoria2_1);
+        categoriasExpected.add(categoria2_1);
+
+        Categoria categoria2_2 = new Categoria();
+        categoria2_2.setNombre("Lindos");
+        categoria2_2.setPadre(categoria2);
+        categoria2.getHijos().add(categoria2_2);
+        categoriasExpected.add(categoria2_2);
+
+        Categoria categoria2_2_1 = new Categoria();
+        categoria2_2_1.setNombre("Caros");
+        categoria2_2_1.setPadre(categoria2_2);
+        categoria2_2.getHijos().add(categoria2_2_1);
+        categoriasExpected.add(categoria2_2_1);
+
+        Categoria categoria2_2_2 = new Categoria();
+        categoria2_2_2.setNombre("Baratos");
+        categoria2_2_2.setPadre(categoria2_2);
+        categoria2_2.getHijos().add(categoria2_2_2);
+        categoriasExpected.add(categoria2_2_2);
+
+        for (Categoria categoria : categoriasExpected) {
+            categoriaController.nueva(categoria);
+        }
+
+        ArrayList<Categoria> categoriasActual = categoriaService.listar();
+
+        assertTrue(categoriasExpected.containsAll(categoriasActual) && categoriasActual.containsAll(categoriasExpected));
+
+        for (Categoria categoria : categoriasActual) {
+            assertTrue(categoria.getEstado().equals(EstadoCategoria.visible));
+        }
+
+        categoriaController.ocultar(categoria1);
+
+        assertTrue(categoria1.getEstado().equals(EstadoCategoria.oculta));
+        assertTrue(categoria1_1.getEstado().equals(EstadoCategoria.oculta));
+        assertTrue(categoria1_2.getEstado().equals(EstadoCategoria.oculta));
+
+
+    }
 }
