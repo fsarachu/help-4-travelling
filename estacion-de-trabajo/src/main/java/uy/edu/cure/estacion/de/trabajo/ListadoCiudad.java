7f1552857bc543f46c03d98caf8439f916ad6a64
Victor Torterola <tricovictor@gmail.com>
2016-09-19 22:25:48 -0300

    Listado Ciudad con Clientes

--- estacion-de-trabajo/src/main/java/uy/edu/cure/estacion/de/trabajo/ListadoCiudad.java
+++ estacion-de-trabajo/src/main/java/uy/edu/cure/estacion/de/trabajo/ListadoCiudad.java
@@ -1,14 +1,18 @@
 package uy.edu.cure.estacion.de.trabajo;
 
 import uy.edu.cure.servidor.central.dto.Ciudad;
+import uy.edu.cure.servidor.central.dto.Cliente;
 import uy.edu.cure.servidor.central.dto.Producto;
 import uy.edu.cure.servidor.central.dto.Servicio;
 import uy.edu.cure.servidor.central.lib.controlErroresInteface.LlenarCombobox;
+import uy.edu.cure.servidor.central.lib.controllers.ClienteController;
 import uy.edu.cure.servidor.central.lib.controllers.ProductoController;
 
 import javax.swing.*;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,6 +21,8 @@ public class ListadoCiudad {
     private JComboBox cmbCiudades;
     private JPanel panelPrincipal;
     private JList<Servicio> list;
+    private JList<Cliente> listClientes;
+    private DefaultListModel mdllistaCliente;
     private DefaultListModel mdllista;
 
     public ListadoCiudad() {
@@ -33,6 +39,19 @@ public class ListadoCiudad {
                 List<Servicio> servicios = new ArrayList<>(productoController.listarServiciosPorCiudad(ciudad));
                 for (Servicio servicio : servicios) {
                     mdllista.addElement(servicio);
+                }
+            }
+        });
+        list.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                super.mouseClicked(mouseEvent);
+                if (mouseEvent.getClickCount() == 1) {
+                    mdllistaCliente = new DefaultListModel();
+                    listClientes.setModel(mdllistaCliente);
+                    ClienteController clienteController = new ClienteController();
+                    ArrayList<Cliente> clientes = clienteController.listarCompradoresServicio((Servicio)list.getSelectedValue());
+                    mdllistaCliente.addElement(clientes);
                 }
             }
         });
