77e0b8248dcf8bf027ccfb113d4164c7674801b7
Victor Torterola <tricovictor@gmail.com>
2016-10-16 11:52:41 -0300

    Se muestra cantidad en el carrito en la barra de tareas

--- servidor-web/src/main/java/uy/edu/cure/servidor/web/ReservaBean.java
+++ servidor-web/src/main/java/uy/edu/cure/servidor/web/ReservaBean.java
@@ -1,45 +1,42 @@
 package uy.edu.cure.servidor.web;
 
-import jdk.nashorn.internal.objects.NativeUint8Array;
 import uy.edu.cure.servidor.central.dto.Cliente;
 import uy.edu.cure.servidor.central.dto.Reserva;
 import uy.edu.cure.servidor.central.lib.controllers.ReservaController;
 
-import javax.el.ELContext;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
-import javax.faces.context.FacesContext;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 @ManagedBean
 @SessionScoped
-public class ReservaBean implements Serializable{
+public class ReservaBean implements Serializable {
     private Reserva reserva;
+    private Integer cantidadReservas;
     private List<Reserva> reservaList = new ArrayList<>();
     @ManagedProperty(value = "#{loginBean}")
     private LoginBean loginBean;
 
     public ReservaBean() {
-        /*  //invento cosmico http://stackoverflow.com/questions/25209786/get-bean-values-in-another-bean-in-prime-faces
-        ELContext elContext = FacesContext.getCurrentInstance().getELContext();
-        loginBean = (LoginBean) elContext.getELResolver().getValue(elContext, null, "LoginBean");
-        */
-
-        try{
-            cargarReservas(loginBean.getCliente());
-        }catch( NullPointerException e ){
-            throw new NullPointerException("[ReservaBean: loginBean = NULL: cargarReservas(loginBean.getCliente()) ]" + e.getMessage() );
-        }
 
     }
 
+    public void cantidadReservas() {
+        ReservaController reservaController = new ReservaController();
+        ArrayList<Reserva> reservas = reservaController.listar();
+        for (Reserva reserva : reservas) {
+            reservaList.add(reserva);
+        }
+        cantidadReservas = reservaList.size();
+    }
 
     public void setLoginBean(LoginBean loginBean) {
         this.loginBean = loginBean;
     }
+
     public LoginBean getLoginBean() {
         return this.loginBean;
     }
@@ -66,5 +63,13 @@ public class ReservaBean implements Serializable{
     public void cargarReservas(Cliente cliente) {
         ReservaController reservaController = new ReservaController();
         reservaList = reservaController.listarReservasCliente(cliente);
+    }
+
+    public Integer getCantidadReservas() {
+        return cantidadReservas;
+    }
+
+    public void setCantidadReservas(Integer cantidadReservas) {
+        this.cantidadReservas = cantidadReservas;
     }
 }
