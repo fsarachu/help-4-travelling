c3cd1e32b2b1b8e4acebb39619f5510b8f5bc190
Victor Torterola <tricovictor@gmail.com>
2016-10-20 12:40:43 -0300

    Arbol ok

--- servidor-web/src/main/java/uy/edu/cure/servidor/web/TreeManagedBean.java
+++ servidor-web/src/main/java/uy/edu/cure/servidor/web/TreeManagedBean.java
@@ -1,12 +1,10 @@
 package uy.edu.cure.servidor.web;
 
-import org.primefaces.event.NodeExpandEvent;
 import org.primefaces.model.DefaultTreeNode;
 import org.primefaces.model.TreeNode;
 import uy.edu.cure.servidor.central.dto.Categoria;
 import uy.edu.cure.servidor.central.lib.controllers.CategoriaController;
 
-import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import java.util.ArrayList;
@@ -20,8 +18,10 @@ import java.util.List;
 public class TreeManagedBean {
     private List<Categoria> categorias = new ArrayList<>();
     private TreeNode root;
+    private Integer contador = 1;
+    private List<TreeNode> list = new ArrayList<TreeNode>();
 
-    @PostConstruct
+/*    @PostConstruct
     public void buildTree() {
         root = new DefaultTreeNode("Root", null);
         CategoriaController categoriaController = new CategoriaController();
@@ -31,16 +31,53 @@ public class TreeManagedBean {
                 createNode(categoria, root);
             }
         }
+    }*/
+
+    public void construirArbol(Categoria cate, TreeNode arbol) {
+        CategoriaController categoriaController = new CategoriaController();
+        Categoria categoria = categoriaController.obtener(contador);
+        if (contador <= categoriaController.listar().size() && categoria != null) {
+            contador++;
+            if (categoria.getPadre() == null) {
+                TreeNode rama = new DefaultTreeNode(categoria, root);
+                for (int i = 0; i < list.size(); i++) {
+                    if (i == categoria.getId()) {
+                        list.set(i, rama);
+                    }
+                }
+                construirArbol(categoria, rama);
+            } else {
+                for (int i = 0; i < list.size(); i++) {
+                    if (i == categoria.getPadre().getId()) {
+                        arbol = list.get(i);
+                    }
+                }
+                TreeNode rama = new DefaultTreeNode(categoria, arbol);
+                for (int i = 0; i < list.size(); i++) {
+                    if (i == categoria.getId()) {
+                        list.set(i, rama);
+                    }
+                }
+                construirArbol(categoria, rama);
+            }
+        }
     }
 
     public TreeManagedBean() {
+        root = new DefaultTreeNode("Root", null);
+        CategoriaController categoriacontroller = new CategoriaController();
+        Categoria categoria = new Categoria();
+        categoria.setId(1);
+        for (int i = 1; i <= categoriacontroller.listar().size(); i++) {
+            TreeNode nodo = new DefaultTreeNode();
+            list.add(nodo);
+        }
+        construirArbol(categoria, root);
     }
 
 
     private void createNode(Object tag, TreeNode parent) {
         TreeNode node = new DefaultTreeNode(tag, parent);
-        new DefaultTreeNode("hola", node);
-        new DefaultTreeNode("chau", node);
     }
 
 
@@ -60,4 +97,19 @@ public class TreeManagedBean {
         this.categorias = categorias;
     }
 
+    public Integer getContador() {
+        return contador;
+    }
+
+    public void setContador(Integer contador) {
+        this.contador = contador;
+    }
+
+    public List<TreeNode> getList() {
+        return list;
+    }
+
+    public void setList(List<TreeNode> list) {
+        this.list = list;
+    }
 }
