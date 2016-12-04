a5dd34680a645d1348e7678dc2b209f40839b766
Dreamangst <epm.nicolas@gmail.com>
2016-12-04 03:48:09 -0300

    Persistencia de Factura. NullPointerException al persistir ya que no logre encontrar el id de la reserva

--- servidor-central/servidor-central-lib/src/main/java/uy/edu/cure/servidor/central/lib/controllers/FacturaController.java
+++ servidor-central/servidor-central-lib/src/main/java/uy/edu/cure/servidor/central/lib/controllers/FacturaController.java
@@ -1,10 +1,9 @@
 package uy.edu.cure.servidor.central.lib.controllers;
 
-import uy.edu.cure.servidor.central.dto.EstadoReserva;
+import uy.edu.cure.servidor.central.dto.EstadoFactura;
 import uy.edu.cure.servidor.central.dto.Factura;
-import uy.edu.cure.servidor.central.dto.Reserva;
 import uy.edu.cure.servidor.central.lib.servicios.FacturaService;
-import uy.edu.cure.servidor.central.lib.servicios.ReservaService;
+//import uy.edu.cure.servidor.central.lib.servicios.ReservaService;
 import uy.edu.cure.servidor.central.lib.servicios.ServiceFactory;
 
 import java.util.Date;
@@ -12,20 +11,19 @@ import java.util.Date;
 public class FacturaController {
 
     private FacturaService facturaService;
-    private ReservaService reservaService;
+    //private ReservaService reservaService;
 
     public FacturaController(){
         this.facturaService = ServiceFactory.getFacturaService();
-        this.reservaService = ServiceFactory.getReservaService();
+        //this.reservaService = ServiceFactory.getReservaService();
     }
 
-    public void nueva(Reserva reserva){
-        Factura factura = new Factura();
-        factura.setId(this.facturaService.nextId());
+    public void nueva(Factura factura){
+
         factura.setFecha(new Date());
-        //factura.setEstado(EstadoReserva.facturada);
-        //factura.setReserva(reserva);
-        this.facturaService.agregar(factura.getId(), factura);
+        factura.setEstado(EstadoFactura.facturada);
+
+        this.facturaService.agregar(-1, factura);
     }
 
 }
--- servidor-central/servidor-central-lib/src/main/java/uy/edu/cure/servidor/central/lib/servicios/memoria/FacturaServiceImpl.java
+++ servidor-central/servidor-central-lib/src/main/java/uy/edu/cure/servidor/central/lib/servicios/memoria/FacturaServiceImpl.java
@@ -3,8 +3,12 @@ package uy.edu.cure.servidor.central.lib.servicios.memoria;
 import java.sql.*;
 import java.util.Properties;
 
+import jdk.nashorn.internal.ir.JumpToInlinedFinally;
 import uy.edu.cure.servidor.central.dto.Factura;
+import uy.edu.cure.servidor.central.dto.Reserva;
 import uy.edu.cure.servidor.central.lib.servicios.FacturaService;
+import uy.edu.cure.servidor.central.lib.servicios.ReservaService;
+import uy.edu.cure.servidor.central.lib.servicios.conexion;
 
 /**
  * 
@@ -32,6 +36,7 @@ import uy.edu.cure.servidor.central.lib.servicios.FacturaService;
  * }
  */
 public class FacturaServiceImpl extends GenericServiceImpl<Factura> implements FacturaService {
+
 	private static FacturaServiceImpl ourInstance = new FacturaServiceImpl();
 
 	public static FacturaServiceImpl getInstance() {
@@ -42,34 +47,105 @@ public class FacturaServiceImpl extends GenericServiceImpl<Factura> implements F
 		super();
 	}
 
-	@Override
-	public void agregar(Integer id, Factura factura) {
-		//String url = "jdbc:postgresql://localhost/cure-pa";
-        String url = "jdbc:postgresql://127.0.0.1:5432/curepa";
-
+	private Connection getConnection() throws ClassNotFoundException, SQLException {
+        //String url = "jdbc:postgresql://localhost/cure-pa";
+		String url = "jdbc:postgresql://127.0.0.1:5432/curepa";
 		Properties props = new Properties();
 		props.setProperty("user", "postgres");
 		props.setProperty("password", "1234");
-
-        Statement stmt = null;
-        String query = "INSERT INTO facturas VALUES (2, 'soy un string')";
-
+		Connection connection = null;
 		try {
-            Class.forName("org.postgresql.Driver");
-			Connection con = DriverManager.getConnection(url, props);
-
-            stmt = con.createStatement();
-            //ResultSet rs = stmt.executeQuery(query);
-            Boolean rs = stmt.execute(query);
-
-
-
-
+			Class.forName("org.postgresql.Driver");
+			connection = DriverManager.getConnection(url, props);
+            connection.setAutoCommit(false);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			throw new ClassNotFoundException("Class.forName(\"org.postgresql.Driver\")") ;
 		} catch (SQLException e) {
 			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
+			throw new SQLException("DriverManager.getConnection(url, props)");
+		}
+		return connection;
+	}
+
+	@Override
+	public void agregar(Integer id, Factura factura) {
+        PreparedStatement preparedStatement = null;
+        Connection connection = null;
+        try {
+            connection = getConnection();
+            /* Identificador numérico, la fecha de generada, el identificador de la reserva correspondiente,
+                    el monto total de la reserva y el nick del cliente que la hizo.
+               Además, interesa conocer la cantidades, precios y nombres de los servicios/promociones involucrados en la reserva,
+                    así como los nombres de los proveedores.
+            */
+            String statement = "INSERT INTO facturas (fecha, reserva, estado) VALUES (?, ?, ?)";
+            preparedStatement = connection.prepareStatement( statement );
+                java.sql.Date sqlDate = new java.sql.Date( factura.getFecha().getTime() );
+            preparedStatement.setDate( 1, sqlDate );
+            if(factura.getReserva() != null){ //ya se lo asqueroso de esto
+                if(factura.getReserva().getId() != null){
+                    preparedStatement.setLong( 2, factura.getReserva().getId() );
+                }else{
+                    //exception
+                }
+            }
+            preparedStatement.setString( 3, factura.getEstado().toString()  );
+
+            Boolean result = preparedStatement.execute();
+            connection.commit();
+        } catch (ClassNotFoundException e1) {
+            e1.printStackTrace();
+        } catch (SQLException e) {
+            try {
+                connection.rollback();
+            } catch (SQLException e2) {
+                e2.printStackTrace();
+            }
             e.printStackTrace();
+		}finally{
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
+    }
+    public Factura getFacturaXidReserva(Reserva reserva){
+        return getFacturaXidReserva( reserva.getId());
+
+    }
+    public Factura getFacturaXidReserva(Integer idReserva){
+        Factura factura = null;
+
+
+
+        Reserva reserva = null;
+        reserva = ReservaServiceImpl.getInstance().obtener( idReserva );
+        PreparedStatement preparedStatement = null;
+        Connection connection = null;
+        try {
+            connection = getConnection();
+            /*String statement = "INSERT INTO facturas VALUES (null, ?, ?, ?)";
+            preparedStatement = connection.prepareStatement( statement );
+            preparedStatement.setDate(1, (Date)factura.getFecha() );
+            preparedStatement.setLong( 2, factura.getReserva().getId() );
+            preparedStatement.setString( 3, factura.getEstado().toString()  );
+
+
+            Boolean result = preparedStatement.execute();*/
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally{
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return factura;
     }
 
 	@Override
